2465a994684ce116ab0b615ec29db97e6223fabc
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-13 20:53:32 -0400

    added route for each components and put router-outlet to be able to get components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CrisisListComponent} from './crisis-list/crisis-list.component';
+import {HeroesListComponent} from './heroes-list/heroes-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'crisis-list', component: CrisisListComponent},
+  {path: 'heroes-list', component: HeroesListComponent}
+];
 
 @NgModule({
+  // forRoot() -> to ensures that the app only instantiates one RouterModule
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })

ebfcb56d9965dd351fa3ea710e006015a1c22bb8
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-15 19:55:22 -0400

    added crisisService and messageServic for crisis-center directory

--- src/app/crisis-center/crisis.service.ts
+++ src/app/crisis-center/crisis.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Crisis } from './Crisis';
+import { CRISES } from './mock-crises';
+import {MessageService} from './message.service';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CrisisService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getCrises(): Observable<Crisis[]> {
+    // TODO: send the message _after_ fetching the crises
+    this.messageService.add('CrisisService: fetched crises');
+    return of(CRISES);
+  }
+
+  getCrisis(id: number | string) {
+    return this.getCrises().pipe(
+      // (+) before `id` turns the string into a number
+      map((crises: Crisis[]) => crises.find(crisis => crisis.id === +id))
+    );
+  }
 }
+

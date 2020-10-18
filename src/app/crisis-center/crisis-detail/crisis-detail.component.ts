304ba46128f88bcd9b65ce0986eb78d4e4e61831
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-17 21:47:42 -0400

    fixed an issue

--- src/app/crisis-center/crisis-detail/crisis-detail.component.ts
+++ src/app/crisis-center/crisis-detail/crisis-detail.component.ts
@@ -22,7 +22,7 @@ export class CrisisDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe((data: {crisis: Crisis}) => {
-      // this.editName = data.crisis.name;
+      this.editName = data.crisis.name;
       this.crisis = data.crisis;
     });
   }
